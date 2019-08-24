45561b120e249f7b2cbca46d481c4440d2bb3489
Robert McCary <robert.tyler.mccary@gmail.com>
2019-08-24 17:38:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if "#{board[index]}" == " "
+  if "#{board[index]}" == "X" || "O"
+    return true
+  else
     return false
   end
 end
